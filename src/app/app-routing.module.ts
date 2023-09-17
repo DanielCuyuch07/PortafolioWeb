35b71b97329f5e7ffb0f498f559382be0493303a
DanielCuyuch07 <danielproyectos13@gmail.com>
2023-09-17 14:27:04 -0600

    Reset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContenidoPrincipalComponent } from './component/contenido-principal/contenido-principal.component';
+import { MainComponent } from './component/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "" , component:MainComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './component/main/main.component';
+import { ContenidoPrincipalComponent } from './component/contenido-principal/contenido-principal.component';
+import { ContenidoSecundarioComponent } from './component/contenido-secundario/contenido-secundario.component';
+import { ExperienciaHabilidadesComponent } from './component/experiencia-habilidades/experiencia-habilidades.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ContenidoPrincipalComponent,
+    ContenidoSecundarioComponent,
+    ExperienciaHabilidadesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
